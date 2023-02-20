8d48423a9be055d2003948e29192b401e9d6d25b
Hatem Soliman <hatemthedev@gmail.com>
2023-02-20 21:39:32 +0200

    First JAVA program

---
+++ src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Primitive Data Types
+        System.out.println("Hello, World!");
+
+        // Whole Number Data Types
+        byte numOne = 127;
+        short numTwo = 32_000;
+        int numThree = 2_000_000_000;
+        long numFour = 4_000_000_000_000L;
+        
+        System.out.println(numOne);
+        System.out.println(numTwo);
+        System.out.println(numThree);
+        System.out.println(numFour);
+
+        // Fractional Data Types
+        float price = 799.99F;
+        double lilac = 12.566666666666666666666;
+        
+        System.out.println(price);
+        System.out.println(lilac);
+        
+        // Truth-Conditional Data Types
+        boolean isMillionaire = true;
+        System.out.println(isMillionaire);
+
+        // Character (String-like) Data Type
+        char topG = 'H';
+        System.out.println(topG);
+
+        String name = "Hatem";
+        System.out.println(name);
+    }
+}
